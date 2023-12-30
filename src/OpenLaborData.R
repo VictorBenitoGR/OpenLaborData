7e9690a11e9cc8118b09f0350e85de9577c0d8f5
Victor Garcia <victorbenitogr@gmail.com>
2023-12-30 16:38:14 -0600

    Added OpenLaborData.R file

---
+++ src/OpenLaborData.R
@@ -0,0 +1,54 @@
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# * OpenLaborData
+# * https://github.com/VictorBenitoGR/OpenLaborData
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# ! Consider the README notes
+
+# *** PACKAGES *** ------------------------------------------------------------
+
+library(dplyr)       # Data manipulation
+library(ggplot2)     # Data visualization
+library(lubridate)   # Date manipulation
+library(stringr)     # String manipulation
+library(knitr)       # Dynamic reports
+library(rmarkdown)   # Markdown reports
+library(shiny)       # Web applications
+library(plotly)      # Interactive plots
+library(tidyverse) # Data manipulation and visualization
+
+
+# *** DATA IMPORT *** ---------------------------------------------------------
+
+
+
+# *** MAP VISUALIZATION *** ---------------------------------------------------
+
+
+
+# *** RECURRING BRANCH FORMAT *** ---------------------------------------------
+
+
+
+# *** AVERAGE NUMBER OF STAFF *** ---------------------------------------------
+
+
+
+# *** SALES *** ---------------------------------------------------------------
+
+# * Hourly sales volume
+
+
+# * Sales by seniority
+
+
+
+# *** DEMOGRAPHICS *** --------------------------------------------------------
+
+
+
+# *** CONTRACTS *** -----------------------------------------------------------
+
+
+
+# *** EMPLOYEE DEPARTURES *** -------------------------------------------------
